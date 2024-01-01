a9f5797a3e2f47ff9a1f497e68c63110d0fc8c9b
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2024-01-01 19:52:00 -0300

    Rename and organize project (#5)
    * Adding idea to gitignore
    
    * Renaming project and moving it to repo root
    
    * Removing vs config files
    
    * Udating assemblies and namespaces
    
    * Changing project folders in actions
    
    * Updating NUnit to version 4.0
    
    * adding tests project build step
    
    * Removing test project build
    
    * Adding artifact to TestPR
    
    * Fixing NUnit Errors
    
    * Fix in testPR
    
    * Testing test job
    
    * updating NUnit console runner v
    
    * Testing another way of testing
    
    * rolling back last change
    
    * Changing nunit console runner version
    
    * Revert "Fixing NUnit Errors"
    
    This reverts commit 10bb78ca268d9c28e9894f4b8b078ac80213133b.
    
    * Downgrading Nunit to 3.14
    
    * downgrading console runner
    
    * changing unit tests project's c# version to 4 to fix nunit console runner incompatibility
    Fixing unit tests
    
    * adding nunit test adapter
    
    * changing lang version back to 7
    
    * Downgrading nunit test adapter to 4.3
    
    * trying nunit test adapter in runtime project
    
    * Revert "trying nunit test adapter in runtime project"
    
    This reverts commit f6461fcba826b20ec0c78f19e2996f41a537cba3.
    
    * Downgrading Nunit to 3.5 again

---
+++ Projects/HealthSystem.UnitTests/HealthTests.cs
@@ -0,0 +1,234 @@
+﻿using NUnit.Framework;
+
+namespace HealthSystem.UnitTests
+{
+	[TestFixture]
+	public class HealthTests
+	{
+		private const int MaxHP = 10;
+		private const int OverflowingHP = 20;
+		private const int NotKillingDamage = 5;
+		private const int OverkillDamage = 25;
+		private const int MinimumNonZeroHP = 1;
+		private const int NotOverflowingHeal = 5;
+		private const int OverflowingHeal = 25;
+		private Health _health;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_health = new Health(MaxHP);
+		}
+
+		#region Constructor Tests
+
+		[Test]
+		public void Constructor_GivenMaxHealthPoints_HasSameMaxHealthPoints()
+		{
+			AssertThatHealthPointsDontOverflow();
+		}
+
+		[Test]
+		public void Constructor_GivenOnlyMaxHealthPoints_HasSameHealthPoints()
+		{
+			AssertThatHealthPointsDontOverflow();
+		}
+
+		[Test]
+		public void Constructor_GivenOverflowingHealthPoints_HealthPointsAreClamped()
+		{
+			_health = new Health(MaxHP, OverflowingHP);
+			AssertThatHealthPointsDontOverflow();
+		}
+
+		[Test]
+		public void Constructor_GivenOverflowingHealthPointsAndNotAllowedToOverflow_HealthPointsDontOverflow()
+		{
+			_health = new Health(MaxHP, OverflowingHP, false);
+			AssertThatHealthPointsDontOverflow();
+		}
+
+		[Test]
+		public void Constructor_GivenOverflowingHealthPointsAndIsAllowedToOverflow_HealthPointsOverflow()
+		{
+			_health = new Health(MaxHP, OverflowingHP, true);
+			AssertThatHealthPointsOverflow();
+		}
+
+		#endregion
+
+		#region HealthPoints Tests
+
+		[Test]
+		public void HealthPoints_GivenNotAllowedToOverflowAndSetToOverflowingHealthPoints_HealthPointsDontOverflow()
+		{
+			GivenNotAllowedToOverflow();
+			_health.HP = OverflowingHP;
+			AssertThatHealthPointsDontOverflow();
+		}
+
+		[Test]
+		public void HealthPoints_GivenAllowedToOverflowAndSetToOverflowingHealthPoints_HealthPointsDontOverflow()
+		{
+			GivenAllowedToOverflow();
+			_health.HP = OverflowingHP;
+			AssertThatHealthPointsOverflow();
+		}
+
+		#endregion
+
+		#region TakeDamage Tests
+
+		[Test]
+		public void TakeDamage_GivenTakesDamage_HasLessHealthPoints()
+		{
+			const int expectedHealthPoints = MaxHP - NotKillingDamage;
+			_health.TakeDamage(NotKillingDamage);
+			Assert.That(expectedHealthPoints, Is.EqualTo(_health.HP));
+		}
+		[Test]
+		public void TakeDamage_GivenTakesDamage_OnDamageIsCalled()
+		{
+			bool wasCalled = false;
+			_health.OnDamage += (before, after) => wasCalled = true; 
+			_health.TakeDamage(NotKillingDamage);
+			Assert.That(wasCalled);
+		}
+		[Test]
+		public void TakeDamage_GivenTakesDamage_OnDamageGivesCorrectValues()
+		{
+			int correctBefore = _health.HP;
+			int actualBefore = -1;
+			
+			int correctAfter = correctBefore - NotKillingDamage;
+			int actualAfter = -1;
+			
+			_health.OnDamage += (before, after) =>
+			{
+				actualBefore = before;
+				actualAfter = after;
+			};
+			_health.TakeDamage(NotKillingDamage);
+			Assert.That(correctBefore, Is.EqualTo(actualBefore));
+			Assert.That(correctAfter, Is.EqualTo(actualAfter));
+		}
+
+		[Test]
+		public void TakeDamage_GivenKillingDamage_HealthPointsAreZero()
+		{
+			_health.TakeDamage(OverkillDamage);
+			Assert.That(_health.HP, Is.Zero);
+		}
+
+		[Test]
+		public void TakeDamage_GivenKillingDamage_OnDeathIsCalled()
+		{
+			bool wasCalled = false;
+			_health.OnDeath += () => wasCalled = true; 
+			_health.TakeDamage(OverkillDamage);
+			Assert.That(wasCalled);
+		}
+
+		[Test]
+		public void TakeDamage_GivenKillingDamageAndAllowedOverflow_OnDeathIsCalled()
+		{
+			_health.AllowOverflow = true;
+			bool wasCalled = false;
+			_health.OnDeath += () => wasCalled = true; 
+			_health.TakeDamage(OverkillDamage);
+			Assert.That(wasCalled);
+		}
+
+		#endregion
+
+		#region Heal Tests
+			
+			[Test]
+			public void Heal_GivenHeals_HasMoreHP()
+			{
+				_health.HP = MinimumNonZeroHP;
+				const int expectedHealthPoints = MinimumNonZeroHP + NotOverflowingHeal;
+				_health.Heal(NotOverflowingHeal);
+				Assert.That(expectedHealthPoints, Is.EqualTo(_health.HP));
+			}
+			
+			[Test]
+			public void Heal_GivenHeals_OnHealIsCalled()
+			{
+				bool wasCalled = false;
+				_health.OnHeal += (before, after) => wasCalled = true; 
+				_health.Heal(NotOverflowingHeal);
+				Assert.That(wasCalled);
+			}
+			
+			[Test]
+			public void Heal_GivenHeals_OnHealGivesCorrectValues()
+			{
+				_health.HP = MinimumNonZeroHP;
+				int healingValue = NotOverflowingHeal;
+				
+				int correctBefore = _health.HP;
+				int actualBefore = -1;
+				
+				int correctAfter = correctBefore + healingValue;
+				int actualAfter = -1;
+				
+				_health.OnHeal += (before, after) =>
+				{
+					actualBefore = before;
+					actualAfter = after;
+				};
+				
+				_health.Heal(healingValue);
+				
+				Assert.That(correctBefore, Is.EqualTo(actualBefore));
+				Assert.That(correctAfter, Is.EqualTo(actualAfter));
+			}
+			
+			[Test]
+			public void Heal_GivenOverflowingHealAndIsAllowedToOverflow_HealthPointsOverflow()
+			{
+				GivenAllowedToOverflow();
+				_health.Heal(OverflowingHeal);
+				Assert.That(MaxHP + OverflowingHeal, Is.EqualTo(_health.HP));
+			}
+
+			[Test]
+			public void Heal_GivenOverflowingHealAndNotAllowedToOverflow_HealthPointsAreMaxHP()
+			{
+				GivenNotAllowedToOverflow();
+				_health.Heal(OverflowingHeal);
+				AssertThatHealthPointsDontOverflow();
+			}
+
+		#endregion
+			
+		#region Given
+
+		private void GivenAllowedToOverflow()
+		{
+			_health.AllowOverflow = true;
+		}
+
+		private void GivenNotAllowedToOverflow()
+		{
+			_health.AllowOverflow = false;
+		}
+
+		#endregion
+
+		#region Then
+
+		private void AssertThatHealthPointsOverflow()
+		{
+			Assert.That(OverflowingHP, Is.EqualTo(_health.HP));
+		}
+
+		private void AssertThatHealthPointsDontOverflow()
+		{
+			Assert.That(MaxHP, Is.EqualTo(_health.HP));
+		}
+
+		#endregion
+	}
+}
